57bd4e87a876804a51f34bf0cadf744c4d115115
madhukumar21 <ayitimadhukumar@gmail.com>
2026-07-24 10:43:42 +0530

    into dev

--- src/main/java/com/example/cruds/services/CustomerService.java
+++ src/main/java/com/example/cruds/services/CustomerService.java
@@ -16,4 +16,9 @@ public class CustomerService {
         return "Customer has been added successfully";
     }
 
+    public String delete(Long id){
+        customerRepo.deleteById(id);
+        return "Customer deleted successfully";
+    }
+
 }
